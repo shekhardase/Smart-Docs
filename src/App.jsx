74f44a45721c24ddf8afebea862ea51440ec136a
Shekhar <115777486+shekhardase@users.noreply.github.com>
2025-01-09 11:59:26 +0530

    Changes added corredtion in names

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Background from './components/background'
+import Background from './components/Background'
 import Foreground from './components/Foreground'
 import Temp from './components/Temp'
 
